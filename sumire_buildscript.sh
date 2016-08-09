8de7fceda95d20ec82467bdaf1dd8c83faadfa1c
RyTek <rytek1128@outlook.com>
2016-08-09 21:07:22 +0700

    Buildscript sumire & add build process log to .gitignore

---
+++ sumire_buildscript.sh
@@ -0,0 +1,24 @@
+export ARCH=arm64
+export PATH=../aarch64-linux-android-4.9/bin:$PATH
+export CROSS_COMPILE=aarch64-linux-android-
+make sumire-generic-RyTek_defconfig |& tee log_generic.txt
+make -j$(grep -c ^processor /proc/cpuinfo) |& tee -a log_generic.txt
+
+echo "checking for compiled kernel..."
+if [ -f arch/arm64/boot/Image.gz-dtb ]
+then
+
+	echo "DONE"
+	rm -f ../final_files/boot_E6603.img
+
+	../final_files/mkbootimg --kernel arch/arm64/boot/Image.gz-dtb --ramdisk ../final_files/newrd.gz --cmdline "androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-5 dwc3_msm.prop_chg_detect=Y coherent_pool=2M dwc3_msm.hvdcp_max_current=1500 androidboot.selinux=permissive" --base 0x00000000 --pagesize 4096 --ramdisk_offset 0x02000000 --tags_offset 0x01E00000 --output ../final_files/boot_E6603.img
+
+	cd ../final_files/
+
+	if [ -e boot_E6603.img ]
+	then
+		cp boot_E6603.img boot.img
+		zip RyTek_Kernel.zip boot.img
+		rm -f boot.img
+	fi
+fi
